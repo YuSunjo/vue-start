ec782244d99705ae4b22c754259cb8ab71c230d4
yusunjo <tnswh2023@gmail.com>
2021-07-22 07:18:25 +0900

    eventBus, vuex, 기본 문법

---
+++ src/components/TdComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+import EventBus from "@/components/EventBus";
+
+export default {
+  props: {
+    cellData: String,
+    rowIndex: Number,
+    cellIndex: Number,
+  },
+  methods: {
+    onClickTd() {
+      if (this.cellData) return;
+
+      EventBus.$emit('clickTd', this.rowIndex, this.cellIndex); // emit을 적으면 Event.on이 실행됨
+
+    }
+  }
+};
+</script>
---
+++ src/components/TicTacToeVuex/TieTactoe.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>{{turn}}님의 턴입니다.</div>
+    <table-component :table-data="tableData" />
+    <div v-if="winner">{{winner}}님의 승리!</div>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+import TableComponent from './TableComponent';
+import EventBus from './EventBus';
+export default {
+import store from './store';
+  store,
+  components: {
+    TableComponent,
+  },
+  computed: {
+    // winner() {
+    //   return this.$store.state.winner;
+    // },
+    // turn() {
+    //   return this.$store.state.turn;
+    // }
+    ...mapState(['winner', 'turn']),   // 위에 처럼 다 쓰는 것을 mapState로 한번에
+    // ...mapState({
+    //   winner: state => state.winner,
+    //   turn: 'turn',
+    //   winner2(state) {
+    //     return state.winner + this.data;
+    //   }
+    // })
+  },
+  methods: {
+    onChangeData() {
+      // this.tableData[1][0] = 'X'; 작동하지 않음
+      this.$set(this.tableData[1], 0, 'X'); // Vue.set과 동일
+    },
+  },
+  created() {
+    EventBus.$on('clickTd', this.onClickTd);
+  }
+};
+</script>
+
+<style>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid black;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+}
+</style>
